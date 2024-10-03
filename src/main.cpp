f36be1debc035c2b336e86e65ee8cc926229bba1
Ivan Zheng <91576281+FieryFire0218@users.noreply.github.com>
2024-10-03 05:41:03 -0400

    place stones and changed structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,93 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
 using namespace std;
 using namespace sf;
 
-int cellSize = 40;
+const int cellSize = 35;
+const int halfCell = cellSize / 2;
+const int boardSize = 19;
 
-int main(void)
-{
-    RenderWindow window(VideoMode(cellSize * 19, cellSize * 19), "Go/Weiqi/Baduk");
-
-    window.setPosition(Vector2i(250, 80));
-
-    window.setSize(Vector2u(800, 600));
-
-    View view(FloatRect(0, 0, cellSize * 19, cellSize * 19));
-    window.setView(view);
-
-    while (window.isOpen())
-    {
-        Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == Event::Closed)
-                window.close();
+bool isStarPoint(int x, int y) {
+    vector<pair<int, int>> starPoints = {
+        {3, 3}, {3, 9}, {3, 15},
+        {9, 3}, {9, 9}, {9, 15},
+        {15, 3}, {15, 9}, {15, 15}
+    };
+    for (const auto &point : starPoints) {
+        if (point.first == x && point.second == y) {
+            return true;
         }
+    }
+    return false;
+}
 
-        window.clear(Color::Yellow);
-        auto drawBoard = [&]() 
-        {
-            float halfCell = 1.0 * cellSize / 2;
-            for (int y = 0; y < 19; y++) 
-            {
-                Vertex hline[] = {
-                    Vertex(Vector2f(halfCell, halfCell + y * cellSize)),
-                    Vertex(Vector2f(cellSize * 19 - halfCell, halfCell + y * cellSize))
-                };
-                hline[0].color = Color::Black;
-                hline[1].color = Color::Black;
-                window.draw(hline, 2, Lines);
-            } 
-            for (int x = 0; x < 19; x++) {
-                Vertex vline[] = {
-                    Vertex(Vector2f(halfCell + x * cellSize, halfCell)),
-                    Vertex(Vector2f(halfCell + x * cellSize, cellSize * 19 - halfCell))
-                };
-                vline[0].color = Color::Black;
-                vline[1].color = Color::Black;
-                window.draw(vline, 2, Lines);
+void drawBoard(RenderWindow &window) {
+    RectangleShape cell(Vector2f(cellSize, cellSize));
+    cell.setFillColor(Color(181, 136, 99));
+    cell.setOutlineColor(Color::Black);
+    cell.setOutlineThickness(1);
+
+    CircleShape starPoint(0.35 *cellSize / 2.5);
+    starPoint.setFillColor(Color::Black);
+    starPoint.setOutlineColor(Color::Black);
+    starPoint.setOutlineThickness(-2);
+
+    for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) {
+            cell.setPosition(halfCell + i * cellSize, halfCell + j * cellSize);
+            window.draw(cell);
+
+            if (isStarPoint(i, j)) {
+                starPoint.setPosition(halfCell + i * cellSize - starPoint.getRadius(), halfCell + j * cellSize - starPoint.getRadius());
+                window.draw(starPoint);
             }
-            //Start points
-            float startR = cellSize / 6;
-            CircleShape circle(startR);
-            circle.setFillColor(Color::Black);
-            for (int y = 0; y < 3; y++) 
-                for (int x = 0; x < 3; x++) {
-                    circle.setPosition(halfCell + (3+6*x) * cellSize - startR, halfCell + (3+6*y) * cellSize - startR);
-                    window.draw(circle);
-                }
-        };
-        drawBoard();
+        }
+    }
+}
 
-        auto drawStones = [&]() {
-            CircleShape stone(1.0 *cellSize / 2);
-            stone.setFillColor(Color::White);
-            stone.setOutlineColor(Color::Black);
-            stone.setOutlineThickness(-2);
-            
-            stone.setPosition(0, 0);
-            window.draw(stone);
-        };
-        drawStones();
+void placeStone(RenderWindow &window, int x, int y) {
+    CircleShape stone(1.0 *cellSize / 2.5);
+    stone.setFillColor(Color::White);
+    stone.setOutlineColor(Color::Black);
+    stone.setOutlineThickness(-2);
+    
+    stone.setPosition(halfCell + x * cellSize - stone.getRadius(), halfCell + y * cellSize - stone.getRadius());
+    window.draw(stone);
+}
 
+void handleMouseClick(RenderWindow &window, vector<pair<int, int>> &stonePositions) {
+    Event event;
+    while (window.pollEvent(event)) {
+        if (event.type == Event::Closed) {
+            window.close();
+        } else if (event.type == Event::MouseButtonPressed) {
+            if (event.mouseButton.button == Mouse::Left) {
+                Vector2i mousePos = Mouse::getPosition(window);
+                int x = mousePos.x / cellSize;
+                int y = mousePos.y / cellSize;
+
+                stonePositions.push_back(make_pair(x, y));
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    RenderWindow window(VideoMode(700, 1300), "Go/Weiqi/Baduk");
+    window.setPosition(Vector2i(300, 20));
+
+    vector<pair<int, int>> stonePositions;
+
+    while (window.isOpen()) {
+        handleMouseClick(window, stonePositions);
+        window.clear();
+        drawBoard(window);
+        for (const auto &pos : stonePositions) {
+            placeStone(window, pos.first, pos.second);
+        }
         window.display();
     }
-
     return 0;
 }
